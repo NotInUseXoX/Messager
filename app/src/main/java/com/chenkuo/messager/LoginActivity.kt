15b9dcfeb9d430a7c5afbfd526ff9edd7222cf42
陈阔 <chenkuo@chenkuodeMacBook-Pro.local>
2018-09-12 19:17:14 +0800

    Initial commit

---
+++ app/src/main/java/com/chenkuo/messager/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.chenkuo.messager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        login_button_login.setOnClickListener {
+            val email = email_edittext_login.text.toString()
+            val password = password_edittext_login.text.toString()
+
+            Log.d("Login", "Attempt login with email: $email, password: $password")
+        }
+
+        back_to_register_textview.setOnClickListener{
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/chenkuo/messager/RegisterActivity.kt
@@ -0,0 +1,114 @@
+package com.chenkuo.messager
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_register.*
+import java.util.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        register_button_register.setOnClickListener {
+            performRegister()
+        }
+        already_have_account_textview.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+        selectphoto_button_register.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent,0)
+        }
+
+    }
+
+    private var selectedPhotoUri: Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null){
+            Log.d("Register","Photo was selected!")
+
+            selectedPhotoUri = data.data
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver,selectedPhotoUri)
+            selectphoto_imageview_register.setImageBitmap(bitmap)
+            selectphoto_button_register.alpha = 0f
+        }
+    }
+
+    private fun performRegister(){
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()){
+            Toast.makeText(this,"Please enter text in email/password",Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful) {
+                        return@addOnCompleteListener
+                    }
+
+                    Log.d("Register","Successfully created user with uid: ${it.result.user.uid}")
+
+                    uploadImageToFirebaseStorage()
+                }
+                .addOnFailureListener { Toast.makeText(this,"Fail to create account: ${it.message}",Toast.LENGTH_SHORT).show() }
+    }
+
+    private fun uploadImageToFirebaseStorage() {
+        if(selectedPhotoUri == null) return
+
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectedPhotoUri!!)
+                .addOnSuccessListener {
+                    Log.d("Register","Sucessfully upload image: ${it.metadata?.path}")
+
+                    ref.downloadUrl.addOnSuccessListener {
+                        saveUserToFirebaseDatabase(it.toString())
+
+                    }
+                }
+                .addOnFailureListener{
+                    Log.d("Register","Failed to upload image: ${it.message}")
+                }
+
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String){
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, username_edittext_register.text.toString(), profileImageUrl)
+
+        ref.setValue(user)
+                .addOnSuccessListener {
+                    Log.d("Register","Successfully Save data to firebase database")
+                    val intent = Intent(this, LatestMessagesActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    startActivity(intent)
+                }
+                .addOnFailureListener{
+                    Log.d("Register","Fail to save data to firebase database: ${it.message}")
+                }
+    }
+}
+
+class User(val uid: String, val username: String, val profileImageUrl: String)
